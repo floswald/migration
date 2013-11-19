dc143c39c7a7e1d8a1086574e90930ad0d93dca9
Florian Oswald <florian.oswald@gmail.com>
2013-11-19 12:03:50 +0000

    dirty analysis of wage growth for movers in SIPP

--- data/SIPP_analyse.r
+++ data/SIPP_analyse.r
@@ -10,6 +10,7 @@ library(texreg)
 
 
 setwd("~/git/migration/data/")
+dropbox <- "~/Dropbox/mobility/output/data/sipp"
 
 if( !file.exists( "SIPP2008.RData" ) ){
 
@@ -43,7 +44,12 @@ if( !file.exists( "SIPP2008.RData" ) ){
 	d[,duration_at_previous := MIG_year_moved_here - MIG_year_moved_into_previous]
 
 	# make a unique observation number
-	d[,upid := paste0(ssuid,epppnum)]
+	d[,pid := paste0(ssuid,epppnum)]
+
+	# numeric educ variable
+	d[,educx := as.numeric(educ)]
+	d[,college := FALSE]
+	d[educx>10, college := TRUE]
 
 	
 	# make lagged variables
@@ -54,19 +60,36 @@ if( !file.exists( "SIPP2008.RData" ) ){
 	d[,monthid := as.integer(month)]
 	d[,yearmon := year * 100 + monthid]
 
+	# need a sequence of unique year month identifiers
+	tmp <- d[,list(yearmon=unique(yearmon))]
+	tmp <- tmp[complete.cases(tmp)]
+	tmp[,yrmnid := 1:nrow(tmp)]
+	setkey(tmp,yearmon)
+	setkey(d,yearmon)
+
+	d <- d[tmp]
+
 	setkey(d,ssuid,pid,year,monthid)
 	d[,own.1yr      := d[list(ssuid,pid,year-1,monthid)][["own"]]]
 	d[,non.work.1yr := d[list(ssuid,pid,year-1,monthid)][["non.work"]]]
 	d[,numkids.1yr  := d[list(ssuid,pid,year-1,monthid)][["numkids"]]]
 	d[,wealth.1yr   := d[list(ssuid,pid,year-1,monthid)][["wealth"]]]
 	d[,equity.1yr   := d[list(ssuid,pid,year-1,monthid)][["home.equity"]]]
+	d[,income.1yr   := d[list(ssuid,pid,year-1,monthid)][["tftotinc"]]]
 	
-	setkey(d,ssuid,pid,yearmon)
-	d[,own.1mn      := d[list(ssuid,pid,yearmon-1)][["own"]]]
-	d[,non.work.1mn := d[list(ssuid,pid,yearmon-1)][["non.work"]]]
-	d[,numkids.1mn  := d[list(ssuid,pid,yearmon-1)][["numkids"]]]
-	d[,wealth.1mn   := d[list(ssuid,pid,yearmon-1)][["wealth"]]]
-	d[,equity.1mn   := d[list(ssuid,pid,yearmon-1)][["home.equity"]]]
+	setkey(d,ssuid,pid,yrmnid)
+	d[,own.1mn      := d[list(ssuid,pid,yrmnid-1)][["own"]]]
+	d[,non.work.1mn := d[list(ssuid,pid,yrmnid-1)][["non.work"]]]
+	d[,numkids.1mn  := d[list(ssuid,pid,yrmnid-1)][["numkids"]]]
+	d[,wealth.1mn   := d[list(ssuid,pid,yrmnid-1)][["wealth"]]]
+	d[,equity.1mn   := d[list(ssuid,pid,yrmnid-1)][["home.equity"]]]
+	d[,income.1mn   := d[list(ssuid,pid,yrmnid-1)][["tftotinc"]]]
+
+	# make growth variables
+	# =====================
+
+	d[,dincome.mn := tftotinc - income.1mn,by=pid]
+	d[,dincome.yr := tftotinc - income.1yr,by=pid]
 
 
 	# heads only
@@ -80,7 +103,7 @@ if( !file.exists( "SIPP2008.RData" ) ){
 	load("SIPP2008.RData")
 }
 
-
+stop()
 
 f[age>25&age<65,list(moved.X.state=weighted.mean(S2S,famweight))]
 
@@ -90,20 +113,20 @@ p <- list()
 p$all <- ggplot(f[age>25&age<65,list(moved.X.state=weighted.mean(S2S,famweight)),by=list(age)],aes(x=age,y=moved.X.state)) + geom_point() + geom_smooth(size=1.1) + scale_y_continuous(limit=c(0,0.012)) + theme_bw()
 
 # plot of family heads moving or not by tenure
-p$own <- ggplot(f[age>25&age<65,list(moved.X.state=weighted.mean(S2S,famweight),nobs=length(S2S)),by=list(age,own)],aes(x=age,y=moved.X.state,color=own)) + geom_point(aes(size=nobs),alpha=0.7) + geom_smooth(size=1.1)  + scale_y_continuous(limit=c(0,0.012)) + theme_bw()
+p$own <- ggplot(f[age>25&age<65,list(moved.X.state=weighted.mean(S2S,famweight),nobs=length(S2S)),by=list(age,own)],aes(x=age,y=moved.X.state,color=own)) + geom_point(aes(size=nobs)) + geom_smooth(size=1.1)  + scale_y_continuous(limit=c(0,0.012)) + theme_bw() + scale_color_manual(values=c("blue","red"))
 
 
-p$dur <- ggplot(f[age>25&age<65,list(moved.X.state=weighted.mean(S2S,famweight),nobs=length(S2S)),by=list(duration_at_previous)],aes(x=duration_at_previous,y=moved.X.state)) + geom_point(aes(size=nobs),alpha=0.7) + geom_smooth(size=1.1)   + theme_bw()
+p$dur <- ggplot(f[age>25&age<65,list(moved.X.state=weighted.mean(S2S,famweight),nobs=length(S2S)),by=list(duration_at_previous)],aes(x=duration_at_previous,y=moved.X.state)) + geom_point(aes(size=nobs)) + geom_smooth(size=1.1)   + theme_bw() + 
 
-pdf("output/sipp/all.pdf")
+pdf(file.path(dropbox,"all.pdf"))
 print(p$all)
 dev.off()
 
-pdf("output/sipp/own.pdf")
+pdf(file.path(dropbox,"own.pdf"),width=9,height=6.5)
 print(p$own)
 dev.off()
 
-pdf("output/sipp/dur.pdf")
+pdf(file.path(dropbox,"dur.pdf"))
 print(p$dur)
 dev.off()
 
@@ -136,10 +159,22 @@ m2 <- list()
 #m2$svylin <- svyglm(S2S ~ age + I(age^2) + own.1mn + wealth.1mn + equity.1mn + numkids.1mn + non.work.1mn,design=sur$fam)
 #m2$svyprobit <- svyglm(S2S ~ age + I(age^2) + own.1mn + wealth.1mn + equity.1mn + numkids.1mn + non.work.1mn,design=sur$fam, family=quasibinomial(link="probit"),x=TRUE)
 m2$svylogit <- svyglm(S2S ~ age + I(age^2) + own.1mn + wealth.1mn + equity.1mn + numkids.1mn + non.work.1mn,design=sur$fam, family=quasibinomial(link="logit"),x=TRUE)
+m2$Rsquared <- 1-m2$svylogit$deviance/m2$svylogit$null.deviance
 
 #m2$svyprob_marginal <- maBina(m2$svyprobit,digits=5)	
 #m2$svylogit_marginal <- maBina(m2$svylogit,digits=5)	
 
 
-texreg(l=list(m$svylogit),custom.names=c("year lag"),file="output/SIPP_yearly.tex",table=FALSE)
-texreg(l=list(m2$svylogit),custom.names=c("monthly lag"),file="output/SIPP_monthly.tex",table=FALSE)
+texreg(l=list(m$svylogit),custom.names=c("year lag"),file=file.path(dropbox,"SIPP_yearly.tex"),table=FALSE)
+texreg(l=list(m2$svylogit),custom.names=c("monthly lag"),file=file.path(dropbox,"SIPP_monthly.tex"),table=FALSE,digits=3)
+
+# wage growth movers/non-movers
+# =============================
+f[,list(dinc=mean(dincome.mn,na.rm=T)),by=list(college,age,S2S)][,xtabs(dinc ~ age + S2S)]
+tmp <- f[,list(dinc=mean(dincome.mn,na.rm=T)),by=list(age,S2S,college)]
+ggplot(subset(tmp,dinc>-11700),aes(x=age,y=dinc,color=S2S)) + geom_point() + geom_smooth() + facet_wrap(~college)
+
+
+f[,list(dinc=mean(dincome.yr,na.rm=T)),by=list(college,age,S2S)][,xtabs(dinc ~ age + S2S)]
+tmp <- f[,list(dinc=mean(dincome.yr,na.rm=T)),by=list(age,S2S,college)]
+ggplot(subset(tmp,dinc>-11700),aes(x=age,y=dinc,color=S2S)) + geom_point() + geom_smooth() + facet_wrap(~college)
