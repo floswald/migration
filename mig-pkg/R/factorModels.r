c52c0f408d4339e6872830ca06e6dfddb2c20c73
Florian Oswald <florian.oswald@gmail.com>
2014-02-22 17:16:16 +0000

    added factor model

---
+++ mig-pkg/R/factorModels.r
@@ -0,0 +1,231 @@
+
+
+
+
+#' Dynamic Factor Model for Prices
+#'
+#' estimates a dynamic factor model
+dynPrices <- function(dat){
+
+	# demean series
+	dat[,index_sa := .SD[,(index_sa - mean(index_sa))/sd(index_sa)], by=state]
+
+	# bring into right shape
+	m <- reshape(dat[,list(state,quarter,index_sa)],timevar="quarter",idvar="state",direction="wide")
+	st <- m[,state]
+	ti <- dat[,unique(quarter)]
+
+	m[,state:=NULL]
+	m <- as.matrix(m)
+
+	rownames(m) <- st
+	colnames(m) <- ti
+
+	# plot for 6 states
+	sst <- sample(st,6)
+	par(mfcol=(c(3,2)))
+
+	for (i in sst){
+		plot(m[i,],xlab="",ylab="hpi",bty="L",pch=16,col="blue",type="b",main=i)
+	}
+	par(mfcol=c(1,1))
+
+	# setup model
+
+	# number of hidden factors
+	fac <- 3
+	Z3.vals <- list(
+				   "z11",    0,    0,
+				   "z21", "z22",    0,
+				   "z31", "z32","z33",
+				   "z41", "z42","z43",
+				   "z51", "z52","z53",
+				   "z61", "z62","z63",
+				   "z71", "z72","z73",
+				   "z81", "z82","z83",
+				   "z91", "z92","z93",
+				   "z101","z102","z103",
+				   "z111","z112","z113",
+				   "z121","z122","z123",
+				   "z131","z132","z133",
+				   "z141","z142","z143",
+				   "z151","z152","z153",
+				   "z161","z162","z163",
+				   "z171","z172","z173",
+				   "z181","z182","z183",
+				   "z191","z192","z193",
+				   "z201","z202","z203",
+				   "z211","z212","z213",
+				   "z221","z222","z223",
+				   "z231","z232","z233",
+				   "z241","z242","z243",
+				   "z251","z252","z253",
+				   "z261","z262","z263",
+				   "z271","z272","z273",
+				   "z281","z282","z283",
+				   "z291","z292","z293",
+				   "z301","z302","z303",
+				   "z311","z312","z313",
+				   "z321","z322","z323",
+				   "z331","z332","z333",
+				   "z341","z342","z343",
+				   "z351","z352","z353",
+				   "z361","z362","z363",
+				   "z371","z372","z373",
+				   "z381","z382","z383",
+				   "z391","z392","z393",
+				   "z401","z402","z403",
+				   "z411","z412","z413",
+				   "z421","z422","z423",
+				   "z431","z432","z433",
+				   "z441","z442","z443",
+				   "z451","z452","z453",
+				   "z461","z462","z463",
+				   "z471","z472","z473",
+				   "z481","z482","z483",
+				   "z491","z492","z493",
+				   "z501","z502","z503",
+				   "z511","z512","z513")
+	Z3 <- matrix(Z3.vals,nrow=nrow(m),ncol=fac,byrow=TRUE)
+	Q <- B <- diag(1,fac)
+
+	# structure of error matrix on outcome equation
+	R = "diagonal and unequal"
+
+	x0 <- U <- A <- "zero"
+
+	# initial variance
+	V0 <- diag(5,fac)
+
+	# list to pass to MARSS
+	dfa <- list(Z=Z3,A=A,R=R,B=B,U=U,Q=Q,x0=x0,V0=V0)
+	cntl.list <- list(maxit=500,trace=1)
+
+	mod3 <- MARSS(m,model=dfa,control=cntl.list)
+
+
+	# two hidden factors
+	fac <- 2
+	Z2.vals <- list(
+				   "z11",    0, 
+				   "z21", "z22",
+				   "z31", "z32",
+				   "z41", "z42",
+				   "z51", "z52",
+				   "z61", "z62",
+				   "z71", "z72",
+				   "z81", "z82",
+				   "z91", "z92",
+				   "z101","z102",
+				   "z111","z112",
+				   "z121","z122",
+				   "z131","z132",
+				   "z141","z142",
+				   "z151","z152",
+				   "z161","z162",
+				   "z171","z172",
+				   "z181","z182",
+				   "z191","z192",
+				   "z201","z202",
+				   "z211","z212",
+				   "z221","z222",
+				   "z231","z232",
+				   "z241","z242",
+				   "z251","z252",
+				   "z261","z262",
+				   "z271","z272",
+				   "z281","z282",
+				   "z291","z292",
+				   "z301","z302",
+				   "z311","z312",
+				   "z321","z322",
+				   "z331","z332",
+				   "z341","z342",
+				   "z351","z352",
+				   "z361","z362",
+				   "z371","z372",
+				   "z381","z382",
+				   "z391","z392",
+				   "z401","z402",
+				   "z411","z412",
+				   "z421","z422",
+				   "z431","z432",
+				   "z441","z442",
+				   "z451","z452",
+				   "z461","z462",
+				   "z471","z472",
+				   "z481","z482",
+				   "z491","z492",
+				   "z501","z502",
+				   "z511","z512")
+	Z2 <- matrix(Z2.vals,nrow=nrow(m),ncol=fac,byrow=TRUE)
+	Q <- B <- diag(1,fac)
+
+	# structure of error matrix on outcome equation
+	R = "diagonal and unequal"
+
+	x0 <- U <- A <- "zero"
+
+	# initial variance
+	V0 <- diag(5,fac)
+
+	# list to pass to MARSS
+	dfa <- list(Z=Z2,A=A,R=R,B=B,U=U,Q=Q,x0=x0,V0=V0)
+	cntl.list <- list(maxit=50)
+
+	mod2 <- MARSS(m,model=dfa,control=cntl.list)
+
+	return(list(mod2,mod3))
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
--- mig-pkg/R/plots.r
+++ mig-pkg/R/plots.r
@@ -48,3 +48,143 @@ plot.predict.income <- function(n,l){
 }
 
 
+
+#' Plot median incomes
+#'
+plotMedianIncome <- function(saveto="~/Dropbox/mobility/output/data/census"){
+
+	data(US_medinc,package="EconData")
+	data(US_states,package="EconData")
+
+	d <- data.table(medinc.in2012$incl)
+	d[, State := tolower(State)]
+	setnames(d,"State","STATE")
+	setkey(d,STATE)
+
+	s <- US_states[,list(STATE,state,Region,Division)]
+	s[,STATE := tolower(STATE)]
+	s <- s[complete.cases(s)]
+	setkey(s,STATE)
+
+	# merge
+	d <- d[s]
+	d[,Date := as.Date(paste(Year,"01","01",sep="-"))]
+
+
+
+	# aggregate
+	reg <- d[,list(medinc=mean(medinc)),by=list(Region,year(Date))]
+	div <- d[,list(medinc=mean(medinc)),by=list(Division,year(Date))]
+	reg[,Date := as.Date(paste(year,"01","01",sep="-"))]
+	div[,Date := as.Date(paste(year,"01","01",sep="-"))]
+
+	# plots
+	plist <- list()
+	yscale <- scale_y_continuous(name="median annual income in 1000 dollars",limits=c(30,80))
+	yscale2 <- scale_y_continuous(name="median annual income in 1000 dollars")
+
+	plist$regs <- ggplot(reg,aes(x=Date,y=medinc/1000,color=Region,group=Region)) + geom_line(size=1)+ ggtitle('median income income in 2012 dollars')  + yscale2
+	plist$divs <- ggplot(div,aes(x=Date,y=medinc/1000,color=Division)) + geom_line(size=1)+ ggtitle('median income in 2012 dollars')  + yscale2
+
+	plist$divstate <- lapply(d[,unique(Division)], function(x) ggplot(d[Division==x], aes(x=Date,y=medinc/1000,color=STATE)) + geom_line(size=1)+ ggtitle(x) + yscale )
+
+	plist$regstate <- lapply(d[,unique(Region)], function(x) ggplot(d[Region==x], aes(x=Date,y=medinc/1000,color=STATE)) + geom_line(size=1)+ ggtitle(x)  + yscale)
+
+	pdf(file=file.path(saveto,"reg.pdf"))
+	print(plist$regs)
+	dev.off()
+	
+	pdf(file=file.path(saveto,"divs.pdf"))
+	print(plist$divs)
+	dev.off()
+
+	pdf(file=file.path(saveto,"regstate.pdf"),width=20,height=18)
+	do.call(grid.arrange,plist$regstate)
+	dev.off()
+	
+	pdf(file=file.path(saveto,"divstate.pdf"),width=20,height=18)
+	do.call(grid.arrange,plist$divstate)
+	dev.off()
+
+
+
+	return(plist)
+
+}
+	
+
+
+
+
+
+
+
+
+
+#' Plot FHFA house price index series
+#'
+#' group states by region and plot
+#' price series
+#' @param saveto location where to save plots
+#' @return list of ggplots
+plotHousePrices <- function(saveto="~/Dropbox/mobility/output/data/FHFA/"){
+
+	data(FHFA_states,package="EconData")
+
+	d <- states$qtr
+	d[,note:= NULL]
+
+	# group by census division
+
+	data(US_states,package="EconData")
+	US_states[,c("FIPS","STATE","Reg_ID","Div_ID") := NULL]
+
+	setkey(d,state)
+	setkey(US_states,state)
+
+	# merge
+	d <- US_states[d]
+	div <- d[,unique(Division)]
+	reg <- d[,unique(Region)]
+
+	plist <- list()
+
+	# compare divisions
+	dd <- d[,list(index_sa = mean(index_sa)),by=list(Division,Date)]
+	plist$divs <- ggplot(dd,aes(x=Date,y=index_sa,color=Division)) + geom_line(size=1) + theme_bw() + ggtitle('FHFA indices by Census Division') 
+
+	pdf(file=file.path(saveto,"div.pdf"))
+	print(plist$divs)
+	dev.off()
+
+	# compare regions
+	dd <- d[,list(index_sa = mean(index_sa)),by=list(Region,Date)]
+	plist$regs <- ggplot(dd,aes(x=Date,y=index_sa,color=Region)) + geom_line(size=1) + theme_bw() + ggtitle('FHFA indices by Census Region') 
+
+	pdf(file=file.path(saveto,"reg.pdf"))
+	print(plist$regs)
+	dev.off()
+
+	# plots grouping states by divisions
+	yscale <- scale_y_continuous(name="FHFA index",limits=c(85,360))
+
+
+	plist$bydiv <- lapply(div, function(z) ggplot(d[Division==z],aes(x=Date,y=index_sa,color=state)) + geom_line(size=1) + ggtitle( z )+ theme_bw() + yscale)
+
+	pdf(file=file.path(saveto,"bydiv.pdf"),width=14,height=8)
+	do.call(grid.arrange,plist$bydiv)
+	dev.off()
+
+	# plots grouping states by regions
+	plist$byreg <- lapply(reg, function(z) ggplot(d[Region==z],aes(x=Date,y=index_sa,color=state)) + geom_line(size=1) + ggtitle( z )+ theme_bw() + yscale)
+
+	pdf(file=file.path(saveto,"byreg.pdf"),width=14,height=9)
+	do.call(grid.arrange,plist$byreg)
+	dev.off()
+
+
+
+	return(plist)
+
+}
+
