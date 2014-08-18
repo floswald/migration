bb68fb178b96091a5155355626d9f2005b5826f8
Florian Oswald <florian.oswald@gmail.com>
2014-08-18 10:41:53 +0100

    changed bps submit

--- mig/src/cluster/examples/slices.jl
+++ mig/src/cluster/examples/slices.jl
@@ -6,15 +6,6 @@
 require("nodes.jl")
 
 
-# if on my machine test with n points
-# if length(workers())==1
-# 	mig_slice = slices(mprob,2)
-# else
-# 	mig_slice = slices(mprob,length(workers()))
-# end
-
-# plotSlices(mprob,mig_slice[1],mig_slice[2],facet="moments")
-
 mig_slice = slices(mprob,32)
 writetable(joinpath(outdir,"migslice1.csv"),mig_slice[1])
 writetable(joinpath(outdir,"migslice2.csv"),mig_slice[2])
