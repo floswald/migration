7a6278da937a9ff4a70c34fd765116ac232c587f
Florian Oswald <florian.oswald@gmail.com>
2014-08-25 20:28:21 +0100

    changed pbs_slices

---
+++ mig/src/cluster/pbs_slices8.jl
@@ -0,0 +1,17 @@
+
+
+println("Started julia")
+
+include("iridis_launcher.jl")
+
+bind_iridis_procs(8)
+
+require("nodes.jl")
+
+mig_slice = slices(mprob,72)
+writetable(joinpath(outdir,"migslice1.csv"),mig_slice[1])
+writetable(joinpath(outdir,"migslice2.csv"),mig_slice[2])
+
+println("done. quitting cluster.")
+
+quit()
