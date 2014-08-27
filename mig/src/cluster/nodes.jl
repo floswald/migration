56da549e42588f5940a72b2e90f9304ae59ce5d8
Florian Oswald <florian.oswald@gmail.com>
2014-08-28 00:00:07 +0100

    adjust bounds

--- mig/src/cluster/nodes.jl
+++ mig/src/cluster/nodes.jl
@@ -17,7 +17,7 @@ end
 moms = mig.DataFrame(mig.read_rda(joinpath(indir,"moments.rda"))["m"])
 
 # want to estimate those:
-plist = ["gamma","xi1","xi2","omega1","omega2","MC0","MC1","MC3","MC3_2","MC4","Rm","taudist"]
+plist = ["gamma","xi1","xi2","omega2","MC0","MC1","MC3","MC3_2","MC4","Rm","taudist"]
 
 p2 = Dict{ASCIIString,Float64}()
 
@@ -32,15 +32,15 @@ end
 pb = Dict{ASCIIString,Array{Float64,1}}()
 pb["gamma"]  = [1.1,3]
 # pb["lambda"] = [0.0,2]
-pb["xi1"]    = [0.0,0.025]
-pb["xi2"]    = [0.0,0.025]
-pb["omega1"] = [0.0,3]
+pb["xi1"]    = [0.0,0.1]
+pb["xi2"]    = [0.0,0.1]
+# pb["omega1"] = [0.0,3]
 pb["omega2"] = [0.0,3]
 pb["MC0"]    = [0,8]
-pb["MC1"]    = [0.0,1]
+pb["MC1"]    = [0.0,0.5]
 # pb["MC2"]    = [0.0,0.0005]
 pb["MC3"]    = [0,0.25]
-pb["MC3_2"]    = [-0.01,0.1]
+pb["MC3_2"]    = [-0.01,0.025]
 pb["MC4"]    = [0,3]
 pb["Rm"]    = [1.03,1.2]
 pb["taudist"]    = [0.01,0.99]
