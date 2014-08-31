47205cebba3bbe7e4b54e2dc71b4f036402f0879
Florian Oswald <florian.oswald@gmail.com>
2014-08-31 18:14:05 +0100

    d'oh

--- mig/src/cluster/nodes.jl
+++ mig/src/cluster/nodes.jl
@@ -2,7 +2,7 @@
 
 if ENV["USER"] == "florian_o"
 	push!(DL_LOAD_PATH, "/home/florian_o/local/lib")
-elseif ENV["USER"] == eisuc151
+elseif ENV["USER"] == "eisuc151"
 	push!(DL_LOAD_PATH, "/home/eisuc151/local/lib")
 end
 
