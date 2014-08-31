d6d699d63490e2869599e6bd4f9357c970aeed53
Florian Oswald <florian.oswald@gmail.com>
2014-08-31 16:36:20 +0100

    path

--- mig/src/cluster/nodes.jl
+++ mig/src/cluster/nodes.jl
@@ -1,8 +1,11 @@
 
 
-if length(DL_LOAD_PATH)==0
-	push!(DL_LOAD_PATH, "~/local/lib")
+if ENV["USER"] == "florian_o"
+	push!(DL_LOAD_PATH, "/home/florian_o/local/lib")
+elseif ENV["USER"] == eisuc151
+	push!(DL_LOAD_PATH, "/home/eisuc151/local/lib")
 end
+
 
 using mig, MOpt
 
