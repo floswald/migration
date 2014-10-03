62969054f6ce542470410efc34a061e57e5f1248
Florian Oswald <florian.oswald@gmail.com>
2014-10-03 10:57:47 +0100

    moved experiment launcher

--- mig/src/migsrc/experiments.jl
+++ mig/src/migsrc/experiments.jl
@@ -7,6 +7,35 @@
 # 4) moving voucher: moving is too costly
 
 
+function runExperiment(which)
+
+	if ENV["USER"] == "florian_o"
+		push!(DL_LOAD_PATH, "/home/florian_o/local/lib")
+	elseif ENV["USER"] == "eisuc151"
+		push!(DL_LOAD_PATH, "/home/eisuc151/local/lib")
+	end
+
+	# unneccesary?
+	# home = ENV["HOME"]
+	# require(joinpath(home,"git/migration/mig/src/cluster/loadmig.jl"))
+
+	indir, outdir = mig.setPaths()
+
+	if which=="mortgage_deduct"
+		e = mig.exp_Mortgage(true)
+	elseif which=="shockp"
+		e = mig.exp_shockRegion(5,"p")
+	elseif which=="shocky"
+		e = mig.exp_shockRegion(5,"y")
+	end
+
+	using HDF5, JLD
+	save(joinpath(outdir,"exp_$which.JLD"),e)
+	println("done.")
+	return e
+
+end
+
 # age 1 utility difference between 2 policies
 function welfare(ctax::Float64,v0::Float64,opts::Dict)
 	p = Param(2,opts)
