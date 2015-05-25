5d27f7c9924e3e95a247d12886379b33fdea58a8
Florian Oswald <florian.oswald@gmail.com>
2015-05-25 14:32:00 +0100

    print slices when done.

--- mig/src/cluster/pbs_slices8.jl
+++ mig/src/cluster/pbs_slices8.jl
@@ -2,16 +2,16 @@
 
 println("Started julia")
 
-t0 = time()
-
 include("iridis_launcher.jl")
 
 bind_iridis_procs(8)
 
 require("nodes.jl")
 
-	sl = MOpt.slices(mprob,30);
+sl = MOpt.slices(mprob,30);
 
-	MOpt.write(sl,"slices.h5")
+println(sl)
+
+MOpt.write(sl,"slices.h5")
 
 quit()
