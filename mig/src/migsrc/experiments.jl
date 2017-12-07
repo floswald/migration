b81f6cadda11da1944f1b14ef8c048cdb7c1bdb5
Florian Oswald <florian.oswald@gmail.com>
2017-12-07 16:16:09 +0100

    get the same mover effects regardless of which region i shock?!

--- mig/src/migsrc/experiments.jl
+++ mig/src/migsrc/experiments.jl
@@ -737,6 +737,9 @@ function exp_shockRegion(opts::Dict; on_impact::Bool=false)
 	d[which] = sim1
 	flows = getFlowStats(d,false,"$(which)_$j")
 
+	println("att = $att")
+	println("atn = $atn")
+
 
 	out = Dict("which" => which,
 		   "j" => j, 
