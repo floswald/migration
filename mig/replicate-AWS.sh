65be912f2ff101a7f820884e78df988203f8b646
Florian Oswald <florian.oswald@gmail.com>
2019-01-23 11:31:01 +0100

    repl

--- mig/replicate-AWS.sh
+++ mig/replicate-AWS.sh
@@ -1,7 +1,8 @@
 # script to replicate all results in the paper
 
 # this uses cfncluster scpo9
-# created viv `cfncluster create -t t3_9 scpo9`
+# created via `cfncluster create -t t3_9 scpo9`
+# connect via `ssh -i ~/.ssh/aws-paris-scpo.pem ec2-user@52.47.51.233`
 
 julia6 run.jl --help   # show options
 julia6 run.jl --version
